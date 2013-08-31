449ac7e697764f9c6c8b502b621d7b219cb9d0f5
guimodesto <guilherme.modesto@hotmail.com>
2013-08-31 09:53:48 -0300

    Criação da classe estabelecimento

--- Models/Estabelecimento.cs
+++ Models/Estabelecimento.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using Norm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Web;
 
 namespace MenuFacil.Models
 {
     public class Estabelecimento
     {
+        public ObjectId id { get; set; }
+        public string razao_social { get; set; }
+        public string nome_fantasia { get; set; }
+        public PlanoPagamento planoPagamento { get; set; }
+        public string cnpj { get; set; }
+        public string foto { get; set; }
+        public string status { get; set; }
+        public DateTime data_criacao { get; set; }
+        public DateTime data_atualiazacao { get; set; }
+        public Usuario usuario { get; set; }
+
+
     }
 }
